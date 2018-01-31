628744af04c960b04c6255ea6358cd3df3b2e876
Bob Costas <finkzane@gmail.com>
2018-01-31 15:23:10 -0700

    Filled out BaseClass constructor and Copy constructor

--- BaseClass.java
+++ BaseClass.java
@@ -1,14 +1,40 @@
 package p2_Package;
 
 public class BaseClass {
-    public BaseClass( )
-    {
 
+    /**
+     * Default constructor for BaseClass
+     * @param baseSet The base of this class
+     * @param maxDigitSet The maximum amount of digits allowed
+     * @param decValSet The decimal value to be converted to base
+     */
+    public BaseClass( int baseSet, int maxDigitSet, String decValSet )
+    {
+        base = baseSet;
+        maxDigits = maxDigitSet;
+        digitArray = new char[ maxDigits ];
+        initializeDigits();
+        numDigits = 0;
     }
 
+    /**
+     * Copy constructor for BaseClass
+     * @param baseClassToCopy The BaseClass object to be compied to this object
+     */
     public BaseClass( BaseClass baseClassToCopy )
     {
+      int currentChar = 0;
 
+      base = baseClassToCopy.base;
+      maxDigits = baseClassToCopy.maxDigits;
+      numDigits = baseClassToCopy.numDigits;
+      digitArray = new char[ maxDigits ];
+      initializeDigits();
+
+      for( currentChar = 0; currentChar < numDigits; currentChar++) // copy other's array
+      {
+         digitArray[ currentChar ] = digitArray[ currentChar ];
+      }
     }
 
     /**
@@ -19,7 +45,7 @@ public class BaseClass {
     /**
      * base 10 constant used in code
      */
-    private static int BASE_10;
+    private static int BASE_10 = 10;
 
     /**
      * Array for holding digits in the given number
@@ -29,7 +55,7 @@ public class BaseClass {
     /**
      * Higest base value that can be used
      */
-    private static int MAX_BASE_VALUE;
+    private static int MAX_BASE_VALUE = 16;
 
     /**
      * Maximum digits that can be stored in digitArray
