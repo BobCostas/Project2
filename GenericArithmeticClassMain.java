86eefe0e7b1933e963ef759fb6da8ffb7e713c46
Bob Costas <finkzane@gmail.com>
2018-02-03 18:24:12 -0700

    Added testing for GenericArithmeticClass

--- GenericArithmeticClassMain.java
+++ GenericArithmeticClassMain.java
@@ -1,4 +1,18 @@
 package p2_Package;
 
+import java.util.Arrays;
+
 public class GenericArithmeticClassMain {
+    public static void main(String[] args)
+    {
+        BaseClass firstBase = new BaseClass( 3, 10, "200");
+        BaseClass secondBase = new BaseClass( 3, 10, "400");
+        BaseClass thirdBase = new BaseClass( 3, 10, "450");
+        BaseClass fourthBase = new BaseClass( 3, 10, "550");
+
+        GenericArithmeticClass<BaseClass> gac = new GenericArithmeticClass<>( 3 );
+        gac.setValueAt( 0, firstBase );
+        gac.setValueAt( 2, secondBase );
+
+    }
 }
