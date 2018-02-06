1a5b64e6c467501abcd20b4d555ad2076e8a1e8f
Bob Costas <finkzane@gmail.com>
2018-02-06 16:48:33 -0700

    Added testing for insertionSort

--- BaseClassTest.java
+++ BaseClassTest.java
@@ -5,8 +5,23 @@ public class BaseClassTest {
     public static void main(String[] args)
     {
         BaseClass baseClass = new BaseClass( 16, 16, "17532");
-        BaseClass baseClass2 = new BaseClass(16, 16, "45810");
-//        System.out.println(baseClass.compareRegisters( baseClass2 ));
+        BaseClass baseClass2 = new BaseClass(16, 16, "4810");
+        BaseClass baseClass3 = new BaseClass(16, 16, "5810");
+        BaseClass baseClass4 = new BaseClass(16, 16, "4510");
+        BaseClass baseClass5 = new BaseClass(16, 16, "810");
+
+        GenericArithmeticClass<BaseClass> ac = new GenericArithmeticClass<>( 5 );
+        ac.setValueAt(0, baseClass);
+        ac.setValueAt(1, baseClass2);
+        ac.setValueAt(2, baseClass3);
+        ac.setValueAt(3, baseClass4);
+        ac.setValueAt(4, baseClass5);
+
+        ac.runInsertionSort(ac.getCurrentCappacity());
+        for(int i = 0; i < ac.getCurrentCappacity(); i++)
+        {
+            System.out.println(ac.getValueAt(i).toString());
+        }
 
     }
 }
